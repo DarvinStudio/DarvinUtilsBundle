9aa915127faa4da148d041ab3ff7caf5289d6ed8
igor <igor.sv.n@gmail.com>
2015-08-18 16:44:37 +0300

    Add transliterator service.

--- DependencyInjection/DarvinUtilsExtension.php
+++ DependencyInjection/DarvinUtilsExtension.php
@@ -2,6 +2,7 @@
 
 namespace Darvin\UtilsBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -21,7 +22,7 @@ class DarvinUtilsExtension extends Extension
 //        $configuration = new Configuration();
 //        $config = $this->processConfiguration($configuration, $configs);
 
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('transliterator.yml');
     }
 }
