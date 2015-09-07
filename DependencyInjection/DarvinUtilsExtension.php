5eca5a399609bb33a82c7ac2ef181ab248b9cb15
igor <igor.sv.n@gmail.com>
2015-09-07 12:14:34 +0300

    Implement anti-spam form type.

--- DependencyInjection/DarvinUtilsExtension.php
+++ DependencyInjection/DarvinUtilsExtension.php
@@ -31,6 +31,7 @@ class DarvinUtilsExtension extends Extension
 //        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('anti_spam.yml');
         $loader->load('cloner.yml');
         $loader->load('default_value.yml');
         $loader->load('flash.yml');
---
+++ Resources/public/title-fields.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('form .title_field').hide();
+});
