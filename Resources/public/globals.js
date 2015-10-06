56cfdd1fb4dbd92125a553696c1f98e62748c427
igor <igor.sv.n@gmail.com>
2015-10-06 11:36:20 +0300

    Move public scripts from utils bundle to web dir.

--- Resources/public/globals.js
+++
@@ -1,7 +0,0 @@
-var AJAX_LOADER = '<span class="ajax_loader"></span>';
-
-$(document).bind('ajaxComplete', function () {
-    $('.ajax_loader').remove();
-
-    $('a, form').removeData('submitted');
-});
--- Resources/public/title-fields.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $('form .title_field').hide();
-});
