537c2d83afbe93062a6f8cf4827137510ec6bbc0
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-15 11:20:03 +0300

    Pass name to config tree builder.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,8 +25,10 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('darvin_utils');
+        $treeBuilder = new TreeBuilder('darvin_utils');
+
+        /** @var \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
