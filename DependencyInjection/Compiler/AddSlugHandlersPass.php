ff59c1c082b471757a28da410ae6396a5ae38d0f
Igor Nikolaev <igor.sv.n@gmail.com>
2019-02-15 15:09:50 +0300

    Refactor tagged service IDs sorter.

--- DependencyInjection/Compiler/AddSlugHandlersPass.php
+++ DependencyInjection/Compiler/AddSlugHandlersPass.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2015-2018, Darvin Studio
+ * @copyright Copyright (c) 2015-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -23,15 +23,11 @@ class AddSlugHandlersPass implements CompilerPassInterface
     /**
      * {@inheritdoc}
      */
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
-        $ids = $container->findTaggedServiceIds('darvin_utils.slug_handler');
-
-        (new TaggedServiceIdsSorter())->sort($ids);
-
         $manager = $container->getDefinition('darvin_utils.sluggable.manager.entity');
 
-        foreach (array_keys($ids) as $id) {
+        foreach (array_keys((new TaggedServiceIdsSorter())->sort($container->findTaggedServiceIds('darvin_utils.slug_handler'))) as $id) {
             $manager->addMethodCall('addSlugHandler', [new Reference($id)]);
         }
     }
