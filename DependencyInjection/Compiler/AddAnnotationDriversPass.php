6117c419f0fc520099abf3ec6fa2b75ebe17c52a
Igor Nikolaev <igor.sv.n@gmail.com>
2019-12-05 08:40:19 +0300

    Enable strict types.

--- DependencyInjection/Compiler/AddAnnotationDriversPass.php
+++ DependencyInjection/Compiler/AddAnnotationDriversPass.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2015-2018, Darvin Studio
+ * @copyright Copyright (c) 2015-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -22,7 +22,7 @@ class AddAnnotationDriversPass implements CompilerPassInterface
     /**
      * {@inheritdoc}
      */
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         $factory = $container->getDefinition('darvin_utils.mapping.metadata_factory');
 
